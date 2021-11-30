99fac1730bba57ef5566587f93ee710ac78612fa
Fernando Montes <tarearevalo@gmail.com>
2021-11-30 17:26:00 -0600

    Monstrando datos con el DOM

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 }
 console.log(promedio()); */
 
-let paragraphs = document.getElementsByTagName("p");
-console.log(paragraphs);
+const paragraphs = document.getElementsByTagName("p");
+//console.log("Parrafos de este documento: "+paragraphs.length);
 if (paragraphs.length > 0) {
     let paragraph = paragraphs[0];
-    let title = paragraphs[1];
     paragraph.innerText = "Bienvenidos al bootcamp - By TareDev";
-    title.innerText = "Javascript, Node & React";
+    paragraph = "new value";
+    console.log(paragraph);
+}
+
+if (paragraphs.length > 1) {
+    let paragraph = paragraphs[1];
+    //paragraph.innerText = "Parrafos en el documento: " + paragraphs.length;
+    const fecha = new Date();
+    paragraph.innerText = "Parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
 }
