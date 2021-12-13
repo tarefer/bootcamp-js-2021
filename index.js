2f36b7e958a373abb6c8bb3bb6a5a4453b83a03a
Fernando Montes <tarearevalo@gmail.com>
2021-12-13 11:43:28 -0600

    Mi primera aplicacion

--- index.js
+++ index.js
@@ -1,20 +1,127 @@
-/* function promedio() {
-  return 200;
-}
-console.log(promedio()); */
+const form = document.getElementsByTagName("form")[0];
+/**
+ * @type {HTMLInputElement}
+ */
+ const inputCodigo = document.getElementById("codigo");
+ /**
+  * @type {HTMLInputElement}
+  */
+  const inputNombre = document.getElementById("nombre");
+ /**
+  * @type {HTMLInputElement}
+  */
+ const inputCantidad = document.getElementById("cantidad");
+ /**
+  * @type {HTMLInputElement}
+  */
+ const inputPrecio = document.getElementById("precio");
+ /**
+  * @type {HTMLInputElement}
+  */
+const inputCategoria = document.getElementById("categoria");
+const tbody = document.getElementsByTagName("tbody")[0];
+const cantidadTotalElement = document.getElementById("cantidad-totales");
+const precioTotalesElement = document.getElementById("precio-totales");
+const granTotalElement = document.getElementById("gran-total");
 
-const paragraphs = document.getElementsByTagName("p");
-//console.log("Parrafos de este documento: "+paragraphs.length);
-if (paragraphs.length > 0) {
-    let paragraph = paragraphs[0];
-    paragraph.innerText = "Bienvenidos al bootcamp - By TareDev";
-    paragraph = "new value";
-    console.log(paragraph);
+
+let indice = 0;
+let cantidadTotal = 0;
+let precioTotales = 0;
+let granTotal = 0;
+let currentRow;
+
+form.addEventListener("submit", onSubmit);
+
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onSubmit(event){
+
+
+  event.preventDefault();
+
+  const data = new FormData(form);
+  const values = Array.from(data.entries());
+  const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] =  values;
+
+  let codigo = frmCodigo[1];
+  const nombre = frmNombre[1];
+  const cantidad = frmCantidad[1];
+  const precio = frmPrecio[1];
+  const categoria = frmCategoria[1];
+  const total = cantidad * precio;
+
+  cantidadTotal = parseFloat(cantidad)  + cantidadTotal;
+  precioTotales = parseFloat(precio) + precioTotales;
+  granTotal = parseFloat(total) + granTotal;
+
+  let tr;
+  if (!codigo) {
+    indice++;
+    codigo = indice;
+    tr = document.createElement("tr");
+    tbody.appendChild(tr);
+  }else{
+    tr = currentRow;
+  }
+  //indice ++;
+
+  tr.dataset.categoria = categoria;
+  tr.innerHTML = `
+    <td>${codigo}</td>
+    <td>${nombre}</td>
+    <td>${cantidad}</td>
+    <td>${precio}</td>
+    <td>${total}</td>
+    <td><a href="#" onClick="onEdit(event)">Editar</a> | <a href="#" onClick="onDelete(event)">Eliminar</a></td>
+  `;
+
+  
+
+  cantidadTotalElement.innerText = cantidadTotal;
+  precioTotalesElement.innerText = precioTotales;
+  granTotalElement.innerText = granTotal;
+
+  form.reset();
+  inputNombre.focus();
 }
 
-if (paragraphs.length > 1) {
-    let paragraph = paragraphs[1];
-    //paragraph.innerText = "Parrafos en el documento: " + paragraphs.length;
-    const fecha = new Date();
-    paragraph.innerText = "Parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
+/**
+ * 
+ * @param {Event} event 
+ */
+function onEdit(event){
+  event.preventDefault();
+  
+  /** @type {HTMLElement}*/
+  const anchor = event.target;
+  const tr = anchor.parentElement.parentElement;
+  const celdas = tr.getElementsByTagName("td");
+  const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
+
+  inputCodigo.value = tdCodigo.innerText;
+  inputNombre.value = tdNombre.innerText;
+  inputCantidad.value = tdCantidad.innerText;
+  inputPrecio.value = tdPrecio.innerText;
+  inputCategoria.value = tr.dataset.categoria;
+
+  currentRow = tr;
+}
+
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onDelete(event){
+
+  event.preventDefault();
+
+  /** @type {HTMLElement}*/
+  const anchor = event.target;
+  const tr = anchor.parentElement.parentElement;
+  tbody.removeChild(tr);
 }
